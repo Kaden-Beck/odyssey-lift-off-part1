2bdf5cd7114f0e2c2e1f0dc963a273dbf787b6e6
Kaden-Beck <kadenb12@gmail.com>
2026-05-07 16:18:31 -0600

    Finished LiftOff #1

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,12 +1,48 @@
 import React from "react";
+import { useQuery } from "@apollo/client";
 import { Layout } from "../components";
+import QueryResult from "../components/query-result";
+import TrackCard from "../containers/track-card";
+import { gql } from "../__generated__/";
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+  const { loading, error, data } = useQuery(TRACKS);
+
+  if (loading) return "Loading...";
+
+  if (error) return `Error! ${error?.message}`;
+
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Tracks;
